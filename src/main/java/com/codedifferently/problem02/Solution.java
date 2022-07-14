a12b333983a4c2c1ced7204516c3dffdbf326d8f
McKenna <ohara10@purdue.edu>
2022-07-14 08:17:03 -0500

    Problem 2 done

--- src/main/java/com/codedifferently/problem02/Solution.java
+++ src/main/java/com/codedifferently/problem02/Solution.java
@@ -1,7 +1,20 @@
 package com.codedifferently.problem02;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Solution {
     public String howManyLettersDoYouSee(String input){
-        return null;
+        Map<Character, Integer> map = new TreeMap<>();
+        for (int i = 0; i < input.length(); i++) {
+            char current = input.charAt(i);
+            int count = map.getOrDefault(current, 0);
+            map.put(current, ++count);
+        }
+        StringBuilder builder = new StringBuilder();
+        for (Character character : map.keySet()) {
+            builder.append(String.format("%s:%d ", character, map.get(character)));
+        }
+        return builder.toString().trim();
     }
 }
